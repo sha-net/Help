174e3e2c02e3835ab9514d76b8e533f818a1ad81
Shavit Netzer <shavit.netzer@gmail.com>
2018-01-16 08:04:42 +0200

    Update reset-ubuntu-and-boot.sh

--- reset-ubuntu-and-boot.sh
+++ reset-ubuntu-and-boot.sh
@@ -2,5 +2,6 @@ find /boot | grep generic | grep -v 4.4.0-8 | grep -v `uname -r` | xargs rm -f
 dpkg --configure -a
 apt-get update
 apt-get -f install
-apt-get autoremove
+apt-get remove docker-engine -y
+apt-get autoremove -f -y
 puppet agent -t
