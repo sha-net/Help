ee2659044f38dbfc02535a144ad35e5d93117ff2
Shavit Netzer <shavit.netzer@gmail.com>
2018-01-16 13:59:00 +0200

    Update reset-ubuntu-and-boot.sh

--- reset-ubuntu-and-boot.sh
+++ reset-ubuntu-and-boot.sh
@@ -1,4 +1,6 @@
 find /boot | grep generic | grep -v 4.4.0-8 | grep -v `uname -r` | xargs rm -f
+mv /usr/local/bin/docker-compose /usr/local/bin/docker-composeOLD
+apt-get remove linux-image-4.4.0-109* -y
 dpkg --configure -a
 apt-get update
 apt-get -f install
